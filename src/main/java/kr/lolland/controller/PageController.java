91125e9becba97684c338a6572e692ff948be1f3
User <User@host.docker.internal>
2025-08-14 12:17:02 +0900

    .

--- src/main/java/kr/lolland/controller/PageController.java
+++ src/main/java/kr/lolland/controller/PageController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class PageController {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index(Model model) {
+		//return "redirect:/auction";
+		return "auction";
+	}
 
     @RequestMapping(value = "/auction", method = RequestMethod.GET)
     public String auction(Model model) {
